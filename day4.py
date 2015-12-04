01791564351bc9472a0a257f9c1498399e6b36f5
Chris Shucksmith <chris@shucksmith.co.uk>
2015-12-04 08:25:09 +0000

    reuse key prefix, 14 to 11s

--- day4.py
+++ day4.py
@@ -1,10 +1,10 @@
-import hashlib, itertools, sys
-key='ckczppom'
+import hashlib, itertools
+md = hashlib.md5('ckczppom')
 for target in ['0'*5, '0'*6]:
     for count in itertools.count():
-        md = hashlib.md5()
-        md.update(key + str(count))
-        hd = md.hexdigest()
+        md2 = md.copy()
+        md2.update(str(count))
+        hd = md2.hexdigest()
         if hd.startswith(target):
-            print('Target {0} Count {1}, hash {2}'.format(target, count, hd))
+            print('Target {0} count {1}, hash {2}'.format(target, count, hd))
             break
