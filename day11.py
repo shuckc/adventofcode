040f386aee9e819afb3efd92ec512c2959311839
Chris Shucksmith <chris.shucksmith@gsacapital.com>
2015-12-11 10:42:52 +0000

    Day 11: quirky adder_chain password finder

--- day11.py
+++ day11.py
@@ -1,8 +1,9 @@
 
-def plus(args):
+import re
+def adder_chain(args):
 	c = 1
 	out = []
-	for x in reversed(args): 
+	for x in reversed(args):
 		if c > 0:
 			x += 1
 			c = 0
@@ -10,11 +11,21 @@ def plus(args):
 			c = 1
 			x = ord('a')
 		out.insert(0, x)
-	print out
+	#print out
 	return out
 
-inp = 'xx'
-for i in range(8):
-	print inp
-	inp = ''.join(map(chr, plus(map(ord, inp))))
-	
+inp = 'hxbxwxba'
+print(inp)
+passwords = 2
+while passwords > 0:
+	inp = ''.join(map(chr, adder_chain(map(ord, inp))))
+	#print inp
+	if bool(re.match("[iol]", inp)):
+		continue
+	# all possible tripples
+	if not any( ord(x)+2 == ord(y)+1 == ord(z) for x,y,z in zip( inp, inp[1:], inp[2:]) ):
+		continue
+	if len(set([x for x,y in zip( inp, inp[1:]) if x == y])) < 2:
+		continue
+	print "Password " + inp
+	passwords -= 1
